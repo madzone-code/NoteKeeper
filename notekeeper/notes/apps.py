1da080fd4c8393e96e2c7049e15181c407646975
Максим Костров <madzone1987@gmail.com>
2025-04-27 23:00:40 +0300

    pep8

--- notekeeper/notes/apps.py
+++ notekeeper/notes/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class NotesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notes'
-    verbose_name = 'Модель Заметки'  # Отображаемое в админке имя приложения
+    verbose_name = 'Модель Заметки'  # Отображаемое имя приложения в админке
